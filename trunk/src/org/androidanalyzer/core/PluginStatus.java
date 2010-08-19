7c80b4fbf47e8b4807331cb78f181e1c8cdd8649
t.nachev <t.nachev@c36bc7af-d32f-1c06-1abd-5ed63012cae5>
2010-08-19 12:26:05 +0000

    With reference to the new UI

--- trunk/src/org/androidanalyzer/core/PluginStatus.java
+++ trunk/src/org/androidanalyzer/core/PluginStatus.java
@@ -1,88 +1,90 @@
 package org.androidanalyzer.core;
 
-import org.androidanalyzer.core.utils.Logger;
+import java.util.StringTokenizer;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import org.androidanalyzer.R;
 
-/**
- * Description
- * 
- * @author <Georgi Chepilev>
- * @version <1.0.0>
- */
 public class PluginStatus {
-	private static final String TAG = "Analyzer-Core";
-	private static final String PREFS_NAME = "org.androidanalyzer.core.PluginStatus";
-	private static final String SEPARATOR = ":";
-	private Context ctx;
+  
+  public static final int STATUS_PASSED = R.drawable.ok_icon_3;
+  public static final int STATUS_FAILED = R.drawable.failed_icon_3;
+  public static final int STATUS_NOT_RUN = R.drawable.not_run_icon_5;
+  
+  private static final String DELIM = ":";
+  
+  public static String encodeStatus(PluginStatus pluginStatus) {
+    StringBuffer sb = new StringBuffer(pluginStatus.getPluginClass());
+    sb.append(DELIM).append(pluginStatus.getPluginName());
+    sb.append(DELIM).append(pluginStatus.getStatus());
+    sb.append(DELIM).append(pluginStatus.getLastRun());
+    sb.append(DELIM).append(pluginStatus.isEnabled());
+    return sb.toString();
+  }
+  
+  public static final PluginStatus decodeStatus(String pluginStatus) {
+    StringTokenizer sTok = new StringTokenizer(pluginStatus, DELIM, false);
+    String pluginClass = sTok.nextToken();
+    String pluginName = sTok.nextToken();
+    int status = Integer.valueOf(sTok.nextToken());
+    long lastRun = Long.valueOf(sTok.nextToken());
+    boolean enabled = Boolean.valueOf(sTok.nextToken());
+    PluginStatus decoded = new PluginStatus(pluginName, pluginClass, status, lastRun);
+    decoded.setEnabled(enabled);
+    return decoded;
+  }
+  
+  private String pluginName;
+  private String pluginClass;
+  private int status = STATUS_NOT_RUN;
+  long lastRun = -1;
+  boolean enabled = true;
+  
+  public boolean isEnabled() {
+    return enabled;
+  }
 
-	public PluginStatus(Context ctx) {
-		this.ctx = ctx;
-	}
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
 
-	/**
-	 * Return plugin status
-	 * 
-	 * @param className
-	 *          Plugin class name
-	 * @return status Return status for the given class name of the plugin. Value
-	 *         could be "Passed" or "Failed"
-	 */
-	public String getStatus(String className) {
-		if (className == null || className.length() == 0) {
-			Logger.ERROR(TAG, "className is null");
-			return null;
-		}
-		SharedPreferences prefs = ctx.getSharedPreferences(PREFS_NAME, 0);
-		String allInfo = prefs.getString(className, null);
-		Logger.DEBUG(TAG, "Get all info for " + className + ":" + allInfo);
-		String status = allInfo.substring(allInfo.indexOf(SEPARATOR) + 1);
-		Logger.DEBUG(TAG, "Plugin status: " + status);
-		return status;
-	}
+  public String getPluginName() {
+    return pluginName;
+  }
 
-	/**
-	 * Return last run time of the given plugin
-	 * 
-	 * @param className
-	 *          Plugin class name
-	 * @return Return last run time for the given class name of the plugin. Value
-	 *         is in format yyyy_MM_dd-HH_mm_ss
-	 */
-	public String getLastRun(String className) {
-		if (className == null || className.length() == 0) {
-			Logger.ERROR(TAG, "className is null");
-			return null;
-		}
-		SharedPreferences prefs = ctx.getSharedPreferences(PREFS_NAME, 0);
-		String allInfo = prefs.getString(className, null);
-		Logger.DEBUG(TAG, "Get all info for " + className + ":" + allInfo);
-		String lastRun = allInfo.substring(0, allInfo.indexOf(SEPARATOR));
-		Logger.DEBUG(TAG, "Plugin lastRun: " + lastRun);
-		return lastRun;
-	}
+  public void setPluginName(String pluginName) {
+    this.pluginName = pluginName;
+  }
 
-	/**
-	 * 
-	 * @param className
-	 * @param status
-	 * @param lastRun
-	 */
-	public void constructPluginStatus(String className, String status, String lastRun) {
-		if (className == null || className.length() == 0) {
-			Logger.ERROR(TAG, "className is null");
-		}
-		if (status == null || status.length() == 0) {
-			Logger.ERROR(TAG, "status is null");
-		}
-		if (lastRun == null || lastRun.length() == 0) {
-			Logger.ERROR(TAG, "lastRun is null");
-		}
-		SharedPreferences.Editor prefs = ctx.getSharedPreferences(PREFS_NAME, 0).edit();
-		Logger.DEBUG(TAG, "Store [className - status" + SEPARATOR + "lastRun ] -" + className + " - " + status + SEPARATOR + lastRun);
-		prefs.putString(className, status + SEPARATOR + lastRun);
-		prefs.commit();
-	}
+  public String getPluginClass() {
+    return pluginClass;
+  }
+
+  public void setPluginClass(String pluginClass) {
+    this.pluginClass = pluginClass;
+  }
+
+  public int getStatus() {
+    return status;
+  }
+
+  public void setStatus(int status) {
+    this.status = status;
+  }
+
+  public long getLastRun() {
+    return lastRun;
+  }
+
+  public void setLastRun(long lastRun) {
+    this.lastRun = lastRun;
+  }
+
+  public PluginStatus(String pluginName, String pluginClass, int status, long lastRun) {
+    this.pluginName = pluginName;
+    this.pluginClass = pluginClass;
+    this.status = status;
+    this.lastRun = lastRun;
+  }
+  
 
 }
