ad635551c13ae45181e328cdacb9be1473157b43
georgi.chepilev <georgi.chepilev@c36bc7af-d32f-1c06-1abd-5ed63012cae5>
2010-08-31 11:51:37 +0000

    Initial version

---
+++ src/org/androidanalyzer/plugins/locationaccuracy/LocationAccuracyPreferencesManager.java
@@ -0,0 +1,26 @@
+package org.androidanalyzer.plugins.locationaccuracy;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+public class LocationAccuracyPreferencesManager {
+
+	private static final String PREFS_NAME = "org.androidanalyzer.gui.GPSBenchmarkPreferencesManager";
+
+	/*
+	 * Save key value received from GPS Benchmark application
+	 */
+	public static void savePreference(Context ctx, String key, String value) {
+		SharedPreferences prefs = ctx.getSharedPreferences(PREFS_NAME, 0);
+		Editor edit = prefs.edit();
+		edit.putString(key, value);
+		edit.commit();
+	}
+
+	public static String loadStringPreference(Context ctx, String key) {
+		SharedPreferences prefs = ctx.getSharedPreferences(PREFS_NAME, 0);
+		String result = prefs.getString(key, null);
+		return result;
+	}
+}
