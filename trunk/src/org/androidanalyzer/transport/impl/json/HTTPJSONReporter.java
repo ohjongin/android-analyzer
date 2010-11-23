a266ae2f34123cd6858b0532599203d8e3f7377a
georgi.chepilev <georgi.chepilev@c36bc7af-d32f-1c06-1abd-5ed63012cae5>
2010-11-23 15:40:15 +0000

    added http header for encoding - UTF-16

--- trunk/src/org/androidanalyzer/transport/impl/json/HTTPJSONReporter.java
+++ trunk/src/org/androidanalyzer/transport/impl/json/HTTPJSONReporter.java
@@ -1,11 +1,8 @@
 package org.androidanalyzer.transport.impl.json;
 
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.util.Hashtable;
@@ -19,11 +16,9 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpResponseException;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
@@ -43,8 +38,12 @@ public class HTTPJSONReporter extends Reporter {
 	private static final String X_ANDROID_ANALYZER_REPORT_MD5 = "X_ANDROID_ANALYZER_REPORT_MD5";
 	//sent from AA to the backend, if set
 	private static final String X_ANDROID_ANALYZER_USER_UID = "X_ANDROID_ANALYZER_USER_UID";
-	//sent from the backend to AA to inducate the ID of the new report in the database
+	//sent from the backend to AA to indicate the ID of the new report in the database
 	private static final String X_ANDROID_ANALYZER_REPORT_ID  = "X_ANDROID_ANALYZER_REPORT_ID";
+	//sent from the backend to AA to indicate the content is compressed in gzip format
+	private static final String X_ANDROID_ANALYZER_CONTETN_ENCODING ="Content-Encoding";
+	//sent from the backend to AA to indicate the content is encoded in UTF-16
+	private static final String X_ANDROID_ANALYZER_CHARACTER_ENCODING ="Character-Encoding";
 
 	private static final String TAG = "Analyzer-HTTPJSONReporter";
 
@@ -67,7 +66,7 @@ public class HTTPJSONReporter extends Reporter {
 		Logger.DEBUG(TAG, "[send] jObject: " + jObject);
 		StringEntity se = null;
 		try {
-			se = new StringEntity(jObject.toString(), "UTF-8");
+			se = new StringEntity(jObject.toString(), "UTF-16");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
@@ -79,7 +78,8 @@ public class HTTPJSONReporter extends Reporter {
 		String userUID = extra != null ? extra.get(KEY_USER_UID) : null;
 		if (userUID != null)
 		  httpost.setHeader(X_ANDROID_ANALYZER_USER_UID, userUID);
-		httpost.setHeader("Content-Encoding", "gzip");
+		httpost.setHeader(X_ANDROID_ANALYZER_CONTETN_ENCODING, "gzip");
+		httpost.setHeader(X_ANDROID_ANALYZER_CHARACTER_ENCODING, "UTF-16");
 		httpost.setEntity(new ByteArrayEntity(bytes));
 		HttpResponse responseObject = httpclient.execute(httpost, (HttpContext) null);
 		Header reportIDHeader = responseObject.getFirstHeader(X_ANDROID_ANALYZER_REPORT_ID);
